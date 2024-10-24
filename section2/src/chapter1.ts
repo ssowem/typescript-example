c1d1c7cf1152bfd0d4b5c1fc7da2e985d27dce31
lsyeon0825@gmail.com <lsyeon0825@gmail.com>
2024-10-24 19:06:32 +0900

    first commit

---
+++ section2/src/chapter1.ts
@@ -0,0 +1,34 @@
+// number
+let num1: number = 123;
+let num2: number = -123;
+let num3: number = 0.123;
+let num4: number = -0.123;
+let num5: number = Infinity;
+let num6: number = -Infinity;
+let num7: number = NaN;
+
+// string
+let str1: string = "hello";
+let str2: string = 'hello';
+let str3: string = `hello`;
+let str4: string = `hello ${num1}`;
+
+// boolean
+let bool1: boolean = true;
+let bool2: boolean = false;
+
+// null 
+let null1: null = null;
+
+// undefined
+let unde1: undefined = undefined;
+
+
+// 리터럴(=걊) 타입
+
+let numA: 10 = 10; // 변수타입에 값을 정의하면 그 값 외에는 허용X
+// numA = 12;
+
+let strA: "hello" = "hello";
+
+let boolA: true = true;
