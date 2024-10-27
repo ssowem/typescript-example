25fd3c8ee36926b8208f9a328045cdf4ba32086b
lsyeon0825@gmail.com <lsyeon0825@gmail.com>
2024-10-27 22:22:19 +0900

    test

---
+++ section2/dist/chapter3.js
@@ -0,0 +1,9 @@
+// object
+let user = {
+    id: 1,
+    name: "이소연",
+};
+user = {
+    name: "홍길동",
+};
+export {};
---
+++ section2/dist/chapter4.js
@@ -0,0 +1,23 @@
+let user = {
+    id: 1,
+    name: "이소연",
+    nickname: "sso",
+    birth: "1992.11.04",
+    bio: "안녕하세요",
+    location: "고양시",
+};
+let user2 = {
+    id: 2,
+    name: "이소연",
+    nickname: "sso",
+    birth: "1992.11.04",
+    bio: "안녕하세요",
+    location: "고양시",
+};
+let countryCodes = {
+    Korea: 'ko',
+    UnitedState: 'us',
+    UnitedKingdom: 'uk',
+};
+let countryNumberCodes = {};
+export {};
